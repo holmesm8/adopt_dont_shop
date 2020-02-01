9f9795ebcdd8e29ee00cdcb438958de6e4b7de39
Matt Holmes <holmesm8@gmail.com>
2020-02-01 16:26:47 -0700

    Finish user story 17

--- spec/features/pets/pets_index_spec.rb
+++ spec/features/pets/pets_index_spec.rb
@@ -132,3 +132,44 @@ RSpec.describe "pets index page", type: :feature do
     end
   end
 end
+
+RSpec.describe "pets index page", type: :feature do
+  context 'as a visitor' do
+    it "can select shelter name and go to their show page" do
+      shelter1 = Shelter.create(name: "Mike's Shelter",
+                                address: "1331 17th Street",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80202")
+
+      shelter2 = Shelter.create(name: "Meg's Shelter",
+                                address: "150 Main Street",
+                                city: "Hershey",
+                                state: "PA",
+                                zip: "17033")
+
+      pet1 = Pet.create!(image: "https://cdn2-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg",
+                        name: "Athena",
+                        description: "butthead",
+                        age: "1",
+                        sex: "female",
+                        adoptable: "yes",
+                        shelter_id: shelter1.id
+                        )
+      pet2 = Pet.create!(image: "https://s.abcnews.com/images/Lifestyle/puppy-ht-3-er-170907_16x9_992.jpg",
+                        name: "Odell",
+                        description: "good dog",
+                        age: "4",
+                        sex: "male",
+                        adoptable: "yes",
+                        shelter_id: shelter2.id
+                        )
+
+      visit "/pets"
+      within "#pet-#{pet1.id}" do
+        click_link "#{pet1.shelter.name}"
+      end
+      expect(current_path).to eq("/shelters/#{shelter1.id}")
+    end
+  end
+end
