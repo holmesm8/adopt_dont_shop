959b702da1d003e50dde2f73bbcfc2962c876109
Matt Holmes <holmesm8@gmail.com>
2020-02-01 15:00:59 -0700

    Finish user story 14

--- spec/features/shelters/shelters_index_spec.rb
+++ spec/features/shelters/shelters_index_spec.rb
@@ -61,7 +61,7 @@ RSpec.describe "shelters index page", type: :feature do
                                 zip: "80202")
 
       visit "/shelters"
-      click_link "Update"
+      click_link "Update Shelter"
 
       expect(current_path).to eq("/shelters/#{shelter1.id}/edit")
       expect(page).to have_content("Edit Shelter")
@@ -72,6 +72,24 @@ RSpec.describe "shelters index page", type: :feature do
 
       expect(current_path).to eq("/shelters/#{shelter1.id}")
       expect(page).to have_content("Matts DoggyDayCare")
+    end
+  end
+end
+
+RSpec.describe "shelters index page", type: :feature do
+  context "as a visitor" do
+    it "can delete a shelter" do
+      shelter1 = Shelter.create(name: "Mike's Shelter",
+                                address: "1331 17th Street",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80202")
+
+      visit "/shelters"
+      click_link "Delete Shelter"
+
+      expect(current_path).to eq("/shelters")
+      expect(page).to have_no_content(shelter1.name)
     end
   end
 end
