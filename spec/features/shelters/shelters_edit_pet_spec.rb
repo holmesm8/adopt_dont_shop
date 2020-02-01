5678de1999e16553859746dc820202cab2fef5ec
Matt Holmes <holmesm8@gmail.com>
2020-02-01 13:06:55 -0700

    Finish user story 12

--- spec/features/shelters/shelters_edit_pet_spec.rb
+++ spec/features/shelters/shelters_edit_pet_spec.rb
@@ -46,3 +46,46 @@ RSpec.describe "pets show page", type: :feature do
     end
   end
 end
+
+RSpec.describe "pets show page", type: :feature do
+  context 'as a visitor' do
+    it "can delete a specific pet" do
+      shelter1 = Shelter.create(name: "Mike's Shelter",
+                                address: "1331 17th Street",
+                                city: "Denver",
+                                state: "CO",
+                                zip: "80202")
+
+      shelter2 = Shelter.create(name: "Meg's Shelter",
+                                address: "150 Main Street",
+                                city: "Hershey",
+                                state: "PA",
+                                zip: "17033")
+
+      pet1 = Pet.create!(image: "https://cdn2-www.dogtime.com/assets/uploads/2011/03/puppy-development.jpg",
+                        name: "Athena",
+                        description: "butthead",
+                        age: "1",
+                        sex: "female",
+                        adoptable: "yes",
+                        shelter_id: shelter1.id
+                        )
+
+      pet2 = Pet.create!(image: "https://s.abcnews.com/images/Lifestyle/puppy-ht-3-er-170907_16x9_992.jpg",
+                        name: "Odell",
+                        description: "good dog",
+                        age: "4",
+                        sex: "male",
+                        adoptable: "yes",
+                        shelter_id: shelter2.id
+                        )
+
+      visit "/pets/#{pet1.id}"
+
+      click_link "Delete Pet"
+      expect(current_path).to eq("/pets")
+
+      expect(page).to have_no_content(pet1)
+    end
+  end
+end
